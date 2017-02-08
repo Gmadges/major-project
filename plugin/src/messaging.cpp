651c208eeea7cd23e2edc23667f15d1ee0b3ebb7
George <George.madges@live.co.uk>
2017-02-08 11:17:13 +0000

    added includes a simple test to the plugin solution

--- plugin/src/messaging.cpp
+++ plugin/src/messaging.cpp
@@ -1,4 +1,5 @@
 #include "messaging.h"
+#include <testTypes.hpp>
 
 Messaging::Messaging(std::string _port)
 	:
@@ -15,8 +16,16 @@ Messaging::~Messaging()
 
 void Messaging::send()
 {
-	zmq::message_t request(5);
-	memcpy(request.data(), "Hello", 5);
+	// create my class
+	TestClass test(0, "TEST");
+
+	// pack a message up
+	msgpack::sbuffer sbuf;
+	msgpack::pack(sbuf, test);
+
+	zmq::message_t request(sbuf.size());
+	std::memcpy(request.data(), sbuf.data(), sbuf.size());
+
 	socket.send(request);
 
 	zmq::message_t reply;
