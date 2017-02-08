2acc467b52ac4f3718d6c14883bb159f1fb4b985
George <George.madges@live.co.uk>
2017-02-08 11:32:36 +0000

    turned send into a template for now because it makes sense. god i hate templates being in the header

--- plugin/src/messaging.cpp
+++ plugin/src/messaging.cpp
@@ -1,5 +1,4 @@
 #include "messaging.h"
-#include <testTypes.hpp>
 
 Messaging::Messaging(std::string _port)
 	:
@@ -12,22 +11,4 @@ Messaging::Messaging(std::string _port)
 
 Messaging::~Messaging()
 {
-}
-
-void Messaging::send()
-{
-	// create my class
-	TestClass test(0, "TEST");
-
-	// pack a message up
-	msgpack::sbuffer sbuf;
-	msgpack::pack(sbuf, test);
-
-	zmq::message_t request(sbuf.size());
-	std::memcpy(request.data(), sbuf.data(), sbuf.size());
-
-	socket.send(request);
-
-	zmq::message_t reply;
-	socket.recv(&reply);
 }
--- plugin/src/messaging.h
+++ plugin/src/messaging.h
@@ -8,8 +8,9 @@ class Messaging
 public:
 	Messaging(std::string _port);
 	~Messaging();
-
-	void send();
+	
+	template<typename T>
+	void send(const T& data);
 
 private:
 	std::string port;
@@ -17,5 +18,23 @@ private:
 	zmq::context_t context;
 	zmq::socket_t socket;
 };
+
+#include <msgpack.hpp>
+
+template<typename T>
+void Messaging::send(const T& data)
+{
+	// pack a message up
+	msgpack::sbuffer sbuf;
+	msgpack::pack(sbuf, data);
+
+	zmq::message_t request(sbuf.size());
+	std::memcpy(request.data(), sbuf.data(), sbuf.size());
+
+	socket.send(request);
+
+	zmq::message_t reply;
+	socket.recv(&reply);
+}
 
 #endif
