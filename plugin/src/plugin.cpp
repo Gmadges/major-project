e90a42e146b393379403dff1454863a307316da1
George <George.madges@live.co.uk>
2017-02-15 12:37:16 +0000

    Merge branch 'master' into updateNode
    # Conflicts:
    #	plugin/src/HackPlugin.vcxproj.filters (reverted from commit e62aeaa46c8396b61a5247511817b0ccd9dc987a)

--- plugin/src/plugin.cpp
+++ plugin/src/plugin.cpp
@@ -8,6 +8,10 @@
 #include "scan.h"
 #include "update.h"
 
+//TEST
+#include "meshOpCmd.h"
+#include "meshOpNode.h"
+
 // initialise our plugin and commands
 
 MStatus initializePlugin(MObject obj)
@@ -28,6 +32,25 @@ MStatus initializePlugin(MObject obj)
 		status.perror("registerCommand");
 	}
 
+	//TESTING
+	status = plugin.registerCommand("meshOp", meshOp::creator);
+	if (!status) 
+	{
+		status.perror("registerCommand");
+		return status;
+	}
+
+	status = plugin.registerNode("meshOpNode",
+									meshOpNode::id,
+									meshOpNode::creator,
+									meshOpNode::initialize);
+	
+	if (!status) 
+	{
+		status.perror("registerNode");
+		return status;
+	}
+
 	return status;
 }
 
@@ -46,6 +69,21 @@ MStatus uninitializePlugin(MObject obj)
 	if (!status)
 	{
 		status.perror("deregisterCommand");
+	}
+
+	//TESTING
+	status = plugin.deregisterCommand("meshOp");
+	if (!status) 
+	{
+		status.perror("deregisterCommand");
+		return status;
+	}
+
+	status = plugin.deregisterNode(meshOpNode::id);
+	if (!status) 
+	{
+		status.perror("deregisterNode");
+		return status;
 	}
 
 	return status;
