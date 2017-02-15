8f598e1c3bea38454e5855ee45ded59c971ccfb2
George <George.madges@live.co.uk>
2017-02-15 21:07:36 +0000

    removing some of that example code

--- plugin/src/plugin.cpp
+++ plugin/src/plugin.cpp
@@ -32,25 +32,6 @@ MStatus initializePlugin(MObject obj)
 		status.perror("registerCommand");
 	}
 
-	//TESTING
-	status = plugin.registerCommand("meshOp", meshOp::creator);
-	if (!status) 
-	{
-		status.perror("registerCommand");
-		return status;
-	}
-
-	status = plugin.registerNode("meshOpNode",
-									meshOpNode::id,
-									meshOpNode::creator,
-									meshOpNode::initialize);
-	
-	if (!status) 
-	{
-		status.perror("registerNode");
-		return status;
-	}
-
 	return status;
 }
 
@@ -69,21 +50,6 @@ MStatus uninitializePlugin(MObject obj)
 	if (!status)
 	{
 		status.perror("deregisterCommand");
-	}
-
-	//TESTING
-	status = plugin.deregisterCommand("meshOp");
-	if (!status) 
-	{
-		status.perror("deregisterCommand");
-		return status;
-	}
-
-	status = plugin.deregisterNode(meshOpNode::id);
-	if (!status) 
-	{
-		status.perror("deregisterNode");
-		return status;
 	}
 
 	return status;
