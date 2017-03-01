a6179d8e3782e661d0c2af8433345a86cb44d01d
George <George.madges@live.co.uk>
2017-03-01 18:46:38 +0000

    calls the mel commands now

--- pythonScripts/userInterface.py
+++ pythonScripts/userInterface.py
@@ -22,21 +22,25 @@ class CreateUI(QWidget):
         super(CreateUI, self).__init__(*args, **kwargs)
         self.setParent(mayaMainWindow)
         self.setWindowFlags(Qt.Window)
+        self.address = 'localhost'
+        self.port = 8080
         self.initUI()
-        self.sendCmd = 'ScanSend'
-        self.updateCmd = 'ReceiveUpdate'
         
     def initUI(self):
         # controls
         self.send_btn = QPushButton('Send', self)
+        self.send_btn.clicked.connect(self.send)
         self.update_btn = QPushButton('Update', self)
+        self.update_btn.clicked.connect(self.update)
 
         self.address_line = QLineEdit()
-        self.address_line.setText('localhost')
+        self.address_line.setText(self.address)
+        self.address_line.textChanged[str].connect(self.addressChanged)
 
         self.port_spin = QSpinBox()
         self.port_spin.setRange(1, 65536)
-        self.port_spin.setValue(8080)
+        self.port_spin.setValue(self.port)
+        self.port_spin.valueChanged[int].connect(self.portChanged)
 
         # layout code
         settings_layout = QFormLayout()
@@ -55,13 +59,20 @@ class CreateUI(QWidget):
         main_layout.addLayout(button_layout)
 
         self.setLayout(main_layout)
+    
+    def addressChanged(self, text):
+        self.address = text
 
+    def portChanged(self, port):
+        self.port = port
 
-    def text_dataChange(self):
-        self.cmd = 'poly' + text + '()'
+    def send(self):
+        sendCmd = 'ScanSend -a ' + self.address + ' -p ' + str(self.port)
+        mel.eval(sendCmd)
         
-    def button_sendFunction(self):
-        mel.eval( self.cmd )
+    def update(self):
+        updateCmd = 'ReceiveUpdate -a ' + self.address + ' -p ' + str(self.port)
+        mel.eval(updateCmd)
             
 def main():
     ui = CreateUI()
